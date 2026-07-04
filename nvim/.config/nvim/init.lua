6ce5133b5d87b7eb9e96a6c2155428b943c0df76
linhcub <levanlinhcube@gmail.com>
2026-07-04 15:30:06 +0700

    feat(nvim): add nvim-tree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,6 +9,27 @@ vim.opt.rtp:prepend(lazypath)
 -- Set map leader key to Space
 vim.g.mapleader = " "
 
--- Initialize lazy.nvim with an empty plugin list for now
-require("lazy").setup({})
+-- Initialize lazy.nvim and configure plugins
+require("lazy").setup({
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false, -- Load immediately on startup
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- Required for file and folder icons
+    },
+    config = function()
+      -- Configure nvim-tree options
+      require("nvim-tree").setup({
+        view = {
+          width = 30,    -- Set sidebar width
+          side = "left", -- Position sidebar on the left side
+        },
+      })
+
+      -- Keymapping: Press Ctrl + n to toggle sidebar
+      vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { silent = true })
+    end,
+  },
+})
 
