f226638faa29058cb43eb3992e3b962d4b2ffb67
linhcub <levanlinhcube@gmail.com>
2026-07-04 15:21:18 +0700

    feat(nvim): add lazy.nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- Bootstrap lazy.nvim (automatically download the plugin manager if missing)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set map leader key to Space
+vim.g.mapleader = " "
+
+-- Initialize lazy.nvim with an empty plugin list for now
+require("lazy").setup({})
+
